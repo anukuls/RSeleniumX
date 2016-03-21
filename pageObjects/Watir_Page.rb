841b89f35e097b5481087f69c200e422aebe3e17
unknown <Ekta.Arora@LP-EktaArora.igglobal.com>
2016-03-21 17:25:02 +0530

    static methods

--- pageObjects/Watir_Page.rb
+++ pageObjects/Watir_Page.rb
@@ -4,7 +4,7 @@ require 'selenium-webdriver'
 #Google Page Object class for POC
 class Watir_Page
   
-  def textfield_Watir(driver)
+  def self.textfield_Watir(driver)
     myWait1 = Selenium::WebDriver::Wait.new(:timeout => 30)  
     myWait1.until { driver.find_element(:id => "entry_1000000") } 
     begin
@@ -15,7 +15,7 @@ class Watir_Page
     return element
   end
   
-  def textarea_Watir(driver)
+  def self.textarea_Watir(driver)
     myWait1 = Selenium::WebDriver::Wait.new(:timeout => 30)  
     myWait1.until { driver.find_element(:id => "entry_1000001") }    
     begin
@@ -26,7 +26,7 @@ class Watir_Page
     return element
   end
   
-  def radiobutton_Watir(driver)
+  def self.radiobutton_Watir(driver)
     myWait1 = Selenium::WebDriver::Wait.new(:timeout => 30)  
     myWait1.until { driver.find_element(:id => "group_1000002_1") }  
     begin
@@ -37,7 +37,7 @@ class Watir_Page
     return element
   end
   
-  def radiobutton_Selenium(driver)
+  def self.radiobutton_Selenium(driver)
     myWait1 = Selenium::WebDriver::Wait.new(:timeout => 30)  
     myWait1.until { driver.find_element(:id => "group_1000002_2") }
     
@@ -49,7 +49,7 @@ class Watir_Page
     return element
   end
   
-  def checkbox_Ruby(driver)
+  def self.checkbox_Ruby(driver)
     myWait1 = Selenium::WebDriver::Wait.new(:timeout => 30)  
     myWait1.until { driver.find_element(:id => "group_1000003_1") }
     
@@ -62,7 +62,7 @@ class Watir_Page
   end
 
   
-  def checkbox_Python(driver)
+  def self.checkbox_Python(driver)
     myWait1 = Selenium::WebDriver::Wait.new(:timeout => 30)  
     myWait1.until { driver.find_element(:id => "group_1000003_3") }
     
@@ -74,7 +74,7 @@ class Watir_Page
     return element
   end
   
-  def Dropdown_Watirpage(driver)
+  def self.Dropdown_Watirpage(driver)
     myWait1 = Selenium::WebDriver::Wait.new(:timeout => 30)  
     myWait1.until { driver.find_element(:id => "entry_1000004") }
     
@@ -86,7 +86,7 @@ class Watir_Page
     return element
   end
   
-  def Submit_Button_Watirpage(driver)
+  def self.Submit_Button_Watirpage(driver)
     myWait1 = Selenium::WebDriver::Wait.new(:timeout => 30)  
     myWait1.until { driver.find_element(:id => "ss-submit") }
     
