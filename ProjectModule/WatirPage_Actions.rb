0c70dea91eda7045bbc347075336624864a1efb4
unknown <Ekta.Arora@LP-EktaArora.igglobal.com>
2016-04-22 16:06:38 +0530

    Reporting folder

--- ProjectModule/WatirPage_Actions.rb
+++ ProjectModule/WatirPage_Actions.rb
@@ -1,10 +1,16 @@
 require 'rubygems'
 require 'selenium-webdriver'
 require '../pageObjects/Watir_Page.rb'
+#gem 'test-unit'
+#require 'test/unit'
+require '../utility/Assertion.rb'
+#require 'test/unit/assertions.rb'
 
 
 class WatirPage_Actions
+  
   def self.Fill_Form(driver)
+    
     begin
       Watir_Page.textfield_Watir(driver).send_keys "Selenium Tutorials";
       Watir_Page.textarea_Watir(driver).send_keys "Selenium test";
@@ -12,7 +18,16 @@ class WatirPage_Actions
       Watir_Page.checkbox_Ruby(driver).click
       #Watir_Page.Dropdown_Watirpage(driver).click
       Watir_Page.Submit_Button_Watirpage(driver).click;  
-   
+      result = (driver.find_element(:tag_name => "body").text.include?("Your response has been recorded"))
+      puts result
+      
+      #if result.to_s == "true"
+       # puts "exists"
+      #else
+       # raise "not exists"
+      #end
+      assert {result == true}
+       driver.close
    rescue Exception => e
       puts e.message
    end
--- testScript/Watir_Page_Fill.rb
+++ testScript/Watir_Page_Fill.rb
@@ -2,22 +2,24 @@ require 'rubygems'
 require 'selenium-webdriver'
 require '../utility/Common_Actions'
 require '../ProjectModule/WatirPage_Actions'
+require 'test/unit'
+require '../utility/Reporting'
 
 include ::Common_Actions
-class Watir_Page_Fill 
-   def Watir_Page()
+
+class Watir_Page_Fill < Test::Unit::TestCase 
+   
+   def test_Watir_Page()
      begin
-     driver = Common_Actions.openBrowser("chrome")
-     driver.navigate.to "http://bit.ly/watir-example"
-     puts "AAAAA"
-     WatirPage_Actions.Fill_Form(driver)
-     puts "BBBBB"
-     rescue Exception => e
+       b = Reporting.new
+       driver = Common_Actions.openBrowser("chrome")
+       driver.navigate.to "http://bit.ly/watir-example"   
+        WatirPage_Actions.Fill_Form(driver)  
+        b.add_to_report("Check Google Home Page","Check The Title in Google Home Page","IE","Login should be successfull","Login is successful","PASS","Title Present")
+        b.add_to_report("Check Google Home Page","Check The Title in Google Home Page","FF","Login should be successful","Login is not successful","FAIL","Title Not Present")
+        rescue Exception => e
        puts e.message
      end
    
    end 
 end
-
-w1 = Watir_Page_Fill.new
-w1.Watir_Page
